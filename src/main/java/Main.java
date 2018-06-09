3dc47d94676ee69140fb484201c049ae2a60b003
jdruk <devjosafa@gmail.com>
2018-06-09 19:24:53 -0300

    exemplo simples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,30 @@
-import br.com.jdruk.models.Pessoa;
+import br.com.jdruk.models.Employee;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Main {
+    private static EntityManager em;
+
     public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory =
-                Persistence.createEntityManagerFactory("PU");
 
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("PU");
+        em = emf.createEntityManager();
 
-        Pessoa p = new Pessoa("OI",1l);
-        entityManager.persist(p);
+        createEmployee(1, "Ravi", "Raj", "Textile");
+        createEmployee(2, "Amit", "Raj", "IT");
+        createEmployee(3, "Nitish", "Kumar", "Marketing");
 
-        entityManager.getTransaction().commit();
-        entityManager.close();
+    }
 
+    private static void createEmployee(int id, String firstName,
+                                       String lastName, String dept) {
+        em.getTransaction().begin();
+        Employee emp = new Employee(id, firstName, lastName, dept);
+        em.persist(emp);
+        em.getTransaction().commit();
     }
 }
 
